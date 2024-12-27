0f409f50a2a3eed0867ab2aea0b6fe086320738d
IlyaOsw <ilya_osw@bk.ru>
2024-12-27 11:54:28 +0200

    Inside look done

--- src/pages/Main/InsideLook/InsideLook.tsx
+++ src/pages/Main/InsideLook/InsideLook.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Image } from "antd";
 import { EyeOutlined } from "@ant-design/icons";
+import { motion } from "framer-motion";
 
 import { DescriptionTitle } from "../../../components/DescriptionTitle/DescriptionTitle";
 import { DescriptionText } from "../../../components/DescriptionText/DescriptionText";
+import {
+  animation,
+  useAnimatedInView,
+} from "../../../hooks/useAnimatedInView ";
 
 import styles from "./InsideLook.module.scss";
 
 export const InsideLook: React.FC = () => {
   const { t } = useTranslation();
+  const { ref, controls } = useAnimatedInView();
 
   const content = [
     {
@@ -38,28 +44,48 @@ export const InsideLook: React.FC = () => {
     <div className={styles.container}>
       <DescriptionTitle text={t("insideLook")} textAlign="center" />
       <DescriptionText text={t("insideLookDescription")} textAlign="center" />
-      {content.map((item, index) => (
-        <div
-          key={index}
-          className={`${styles.row} ${index % 2 !== 0 ? styles.reverse : ""}`}
-        >
-          <div className={styles.info}>
-            <h2 className={styles.title}>{item.title}</h2>
-            <h4 className={styles.description}>{item.description}</h4>
+      <motion.div
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={animation}
+      >
+        {content.map((item, index) => (
+          <div
+            key={index}
+            className={`${styles.row} ${index % 2 !== 0 ? styles.reverse : ""}`}
+          >
+            <div className={styles.info}>
+              <h2 className={styles.title}>{item.title}</h2>
+              <h4 className={styles.description}>{item.description}</h4>
+            </div>
+            <Image.PreviewGroup
+              items={[
+                process.env.PUBLIC_URL +
+                  "/assets/Images/InsideLook/Profile.png",
+                process.env.PUBLIC_URL + "/assets/Images/InsideLook/DGC.png",
+                process.env.PUBLIC_URL +
+                  "/assets/Images/InsideLook/Workout1.png",
+                process.env.PUBLIC_URL +
+                  "/assets/Images/InsideLook/Workout2.png",
+              ]}
+            >
+              <Image
+                src={item.image}
+                preview={{
+                  mask: (
+                    <div className={styles.mask}>
+                      <EyeOutlined />
+                      <span>{t("show")} </span>
+                    </div>
+                  ),
+                  maskClassName: styles.maskClass,
+                }}
+              />
+            </Image.PreviewGroup>
           </div>
-          <Image
-            src={item.image}
-            preview={{
-              mask: (
-                <div className={styles.mask}>
-                  <EyeOutlined />
-                  <span>{t("show")} </span>
-                </div>
-              ),
-            }}
-          />
-        </div>
-      ))}
+        ))}
+      </motion.div>
     </div>
   );
 };
--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -13,7 +13,7 @@ export const ProtectedRoute: React.FC<IProtectedRouteProps> = ({
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 875);
+    }, 900);
 
     return () => clearTimeout(timer);
   }, []);
